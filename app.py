e99827b4395bf909d935d3e5afe7189ea6872474
Raga Markely <lmarkely@alum.mit.edu>
2021-10-03 22:24:00 -0400

    feat: Enable delete

--- app.py
+++ app.py
@@ -48,19 +48,32 @@ def create_todo():
     else:
         return jsonify(body)
 
+
 @app.route("/todos/<todo_id>/set-completed", methods=["POST"])
 def set_completed_todo(todo_id):
     try:
         completed = request.get_json()["completed"]
         print("completed", completed)
         todo = Todo.query.get(todo_id)
-        todo.completed = completed 
+        todo.completed = completed
         db.session.commit()
     except:
         db.session.rollback()
     finally:
         db.session.close()
     return redirect(url_for("index"))
+
+
+@app.route("/todos/<todo_id>/deleted", methods=["DELETE"])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({"success": True})
 
 
 @app.route("/")
